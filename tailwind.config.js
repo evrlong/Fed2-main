89930c72d74fcedd9f13b737459f4fb26a0e5444
evrlong <127796398+evrlong@users.noreply.github.com>
2025-01-13 12:01:59 +0100

    Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./**/*.{html,js}","!./node_modules/**/*"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: [
+    "./index.html", // Legg til spesifik filsti hvis den er i roten
+    "./src/**/*.{html,js}", // Hvis HTML/JS er i src-mappen
+    "!./node_modules/**/*",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
